97f1c4b7d7f97ed62dd860396d31c6c7690c71b6
prabhat <shankarprabhat121@gmail.com>
2025-03-31 16:10:04 +0530

    reuirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'PDFMATE Project',
+    version= '0.0.0',
+    author= 'Prabhat',
+    author_email= 'Prabhat.email',
+    packages= find_packages(),
+    install_requires = []
+
+)
